e5e92f0341b2f6a0365bddb6f56855a6f528663c
donnabarnett-cloud <donnabarnett@live.co.uk>
2025-12-10 19:19:16 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
-        }
+        '@google/genai': '@google/generative-ai',        }
       }
     };
 });
